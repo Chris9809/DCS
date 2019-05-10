b898ce66d4a70667f5777e18e2143ed6d23f19e3
apple <87160265@qq.com>
2019-05-10 11:56:29 +0800

    加入jpython，测试git

--- src/test/java/cn/sdtbu/imaker/test.java
+++ src/test/java/cn/sdtbu/imaker/test.java
@@ -3,6 +3,8 @@ package cn.sdtbu.imaker;
 import cn.sdtbu.imaker.DCS.entity.User;
 import cn.sdtbu.imaker.DCS.service.impl.UserServiceImpl;
 import org.junit.Test;
+import org.omg.CORBA.PUBLIC_MEMBER;
+import org.python.util.PythonInterpreter;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -19,5 +21,10 @@ public class test {
         ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
         UserServiceImpl userService = context.getBean("userService", UserServiceImpl.class);
         userService.log();
+    }
+    @Test
+    public void testPython(){
+        PythonInterpreter interpreter = new PythonInterpreter();
+        interpreter.exec("print 3+2");
     }
 }
